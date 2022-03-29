7fe774fbe62d349bc414e8b2538af34b44ef8923
Vinícius Teixeira <55103014+ViniciusTLR@users.noreply.github.com>
2022-03-28 21:34:24 -0300

    Add files via upload

---
+++ Aula UML/src/aula_UML/Carro.java
@@ -0,0 +1,22 @@
+package aula_UML;
+
+public class Carro {
+	
+		public int porta;
+		private boolean parado;
+		protected String cor;
+		
+		public void mostrar() {
+			System.out.println("quantidade de Portas: "+this.porta);
+			System.out.println("O carro esta: "+this.parado);
+			System.out.println("A cor do carro e: "+this.cor);
+		}
+		
+		
+		public void andar() {
+}
+		
+		private void parado() {
+			
+		}
+}
---
+++ Aula UML/src/aula_UML/Main.java
@@ -0,0 +1,11 @@
+package aula_UML;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro();
+		
+	}
+
+}
